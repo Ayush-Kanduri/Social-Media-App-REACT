9d709f3e55e19db77402f5a305164f4e304da44a
Ayush <ayush.kanduri@gmail.com>
2023-10-14 17:12:17 +0530

    configurations

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,39 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import path from "path";
+import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+	define: {
+		"import.meta.vitest": "undefined",
+	},
+	test: {
+		includeSource: ["src/**/*.test.{js,jsx,ts,tsx}", "src/**/*.{js,jsx,ts,tsx}"],
+		exclude: ["node_modules", "build", "dist", "public", "src/components/ui"],
+		coverage: {
+			reporter: ["html", "text"],
+			exclude: [
+				"node_modules",
+				"build",
+				"dist",
+				"public",
+				"src/components/ui",
+			],
+		},
+		globals: true,
+		environment: "jsdom",
+	},
+	plugins: [react()],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src"),
+		},
+	},
+	server: {
+		open: true,
+		port: 8000,
+	},
+	build: {
+		outDir: "build",
+	},
+	base: "/",
+});
